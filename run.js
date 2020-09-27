9df5c506300f551d8946b5e8f245e596f56ce0f7
bouzaien <mokhles.bouzaien@eurecom.fr>
2020-09-27 16:42:11 +0200

    add timestamps and logs

--- run.js
+++ run.js
@@ -1,26 +1,30 @@
 require('dotenv').config();
 var moment = require('moment'); // used to retreive week number
+require('log-timestamp')(function() { return '[' + moment(new Date()).format('YYYY-MM-DD HH:mm:ss') + '] -'});
 
 const puppeteer = require('puppeteer');
 const schedule = require('node-schedule');
 
-var myArgs = process.argv.slice(2); // node run.js mon
+var myArgs = process.argv.slice(2); // node run.js <param>
 
 if (myArgs[0]=='mon'){
-  var sch = '15 00 18 * * 0'; // scheduled for monday at 18:00:15
+  var sch = '55 59 17 * * 0'; // scheduled for monday at 18:00:15
   var eventID = moment().week() + 134224; // 39 => 134263
+} else if (myArgs[0]=='wed'){
+  var sch = '55 59 19 * * 2'; // scheduled for thursday at 19:15:15
+  var eventID = moment().week() + 134437; // 40 => 134477
 } else if (myArgs[0]=='thu'){
-  var sch = '15 15 19 * * 3'; // scheduled for thursday at 19:15:15
+  var sch = '55 14 19 * * 3'; // scheduled for thursday at 19:15:15
   var eventID = moment().week() + 134621; // 39 => 134660
 } else {
-  var sch = '15 00 18 * * 0';
+  var sch = myArgs[1];
   var eventID = myArgs[0];
 };
 
 var eventURL = 'https://schalter.asvz.ch/tn/lessons/' + eventID.toString();
 
 const enrollASVZ = async (eventURL) => {
-  console.log('Enrollment has started.');
+  console.log('Enrollment for ' + eventID.toString() + ' has started.');
   console.log('Opening browser ...');
   let browser = await puppeteer.launch({
     //userDataDir: "./user_data",
@@ -47,7 +51,7 @@ const enrollASVZ = async (eventURL) => {
   
   await page.waitForSelector('.form-element .form-field');
   
-  console.log('Typing username and password ...');
+  console.log('Logging in as ' + process.env.ETH_USR +  ' ...');
   await page.type('#username', process.env.ETH_USR);
   await page.type('#password', process.env.ETH_PASS);
 
@@ -63,9 +67,11 @@ const enrollASVZ = async (eventURL) => {
   await page.click('button[id="btnRegister"]');
 
   await browser.close();
-  console.log('Enrollment has finished.');
+  console.log('Enrollment for ' + eventID.toString() + ' has finished.');
 };
 
 schedule.scheduleJob(sch, function(){
   enrollASVZ(eventURL);
 });
+
+//console.log(moment().week() + 134224);
